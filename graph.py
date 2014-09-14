9f90d5d4354d545fbd1e1cd535cc069ae3e5e3b6
carlos guzman <guzmca@gmail.com>
2014-09-14 13:01:45 -0400

    Fully working! Choice between using a graph or not and all the rules implemented

--- graph.py
+++ graph.py
@@ -1,41 +1,67 @@
-import matplotlib.pyplot as plt
 
-plt.figure(1)
+plot = True if raw_input("Do you want to plot?(y/n)").lower()=='y' else False
 
-plt.ylabel('points')
-plt.xlabel('turn')
+if plot:
+    import matplotlib.pyplot as plt
 
-plt.axhline(color='black')
+    plt.figure(1)
 
-plt.title('Cabo performances')
+    plt.ylabel('points')
+    plt.xlabel('turn')
+
+    plt.axhline(color='black')
+
+    plt.title('Cabo performances')
+
 
 names = raw_input("Please enter the names of the players\n").split(" ")
-all_names = ""
-scores = []
+scores, fifty = {}, {}
 turn = 1
 
 # Set up the scoring system
-for n in range(len(names)):
-    names[n] = names[n].capitalize()
-    all_names += names[n]+", "
-    scores.append([0])
-all_names = all_names[:-2]
+for name in names:
+    names[names.index(name)] = name.capitalize()
+    scores[name.capitalize()]=[0]
+    fifty[name.capitalize()] = False
 
 colors = ['r', 'g', 'b', 'm', 'y', 'k', 'w']
 
 
-# while(for i in scores: i[-1]<=100):
-#     current = raw_input(all_names)
+over = False
+while(not over):
+    print "\nTurn ", turn, "\n"
+    for name in names:
+        scores[name].append(
+            scores[name][-1]+
+            int(raw_input("Please enter the score of "+name+"\n")))
 
-for i in range(len(names)):
-    plt.plot(range(turn), scores[i], colors[i]+'D-', label=names[i])
+        if not fifty[name] and (scores[name][-1]==50 or scores[name][-1]==100):
+            scores[name][-1] -= 50
+            fifty[name] = True
+    
+    if plot:
+        i=0
+        for name, score in scores.iteritems():
+            plt.plot(range(turn+1), score, colors[i]+'D-', label=name)
+            i+=1
+
+        plt.ylim(-5, 100)
+
+        plt.legend(loc=0)
+
+        plt.show()
+        # raw_input()
+        plt.close()
+
+    turn+=1
+
+    # Check whether the game is over
+    for score in scores.values(): 
+        over = over or score[-1]>100
+    
+
+
 
 # plt.plot(xrange(11), [0, 4, 9, 8, 12, 23, 23, 41, 41, 40, 39], 'bD-', label='Carlos')
 # plt.plot(xrange(11), [0, -1, 0, 9, 9, 30, 51, 56, 62, 69, 81], 'gD-', label='Emma')
 # plt.plot(xrange(11), [0, 6, 36, 0, 4, 4, 11, 11, 17, 23, 28], 'rD-', label='Natalie')
-
-plt.ylim(-5, 100)
-
-plt.legend(loc=0)
-
-plt.show()
